84a41a17caaf899ce61c6d7082d17eca85947570
rishalkv <rishallkv@mgsigma.net>
2026-05-26 15:27:39 +0530

    feat: functions

---
+++ functions.py
@@ -0,0 +1,12 @@
+def square(value: int) -> int:
+    """Returns the square of a number."""
+    return value ** 2
+
+
+def cube(value: int) -> int:
+    """Returns the cube of a number."""
+    return value ** 3
+
+
+print(square(5))  # Output: 25
+print(cube(5))    # Output: 125
