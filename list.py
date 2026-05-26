449b8baab37fc83e80a1f57de64ceb55fb8ad0df
rishalkv <rishallkv@mgsigma.net>
2026-05-26 15:24:57 +0530

    feat: list methods

--- list.py
+++ list.py
@@ -1,2 +1,14 @@
-fruits = ("hello", "world", "python")
-fruits[0] = "hi"  # This will raise a TypeError because tuples are immutable
+#append() method is used to add an element at the end of the list.
+numbers =[1,2,3,4,5]
+numbers.append(6)
+print(numbers)
+
+
+#extend() method is used to add multiple elements at the end of the list.
+numbers = [1, 2, 3, 4, 5]
+numbers.extend([6, 7, 8])
+print(numbers)
+
+numbers = [1, 2, 3, 4, 5]
+numbers.insert(0, 10) #insert() method is used to add an element at a specific index in the list.
+print(numbers)
